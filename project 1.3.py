15ac5278565c79eccd21977765d209839b0fcaa6
Dhuwaragan <136139350+Dhuwaragan@users.noreply.github.com>
2023-06-12 20:11:44 +0530

    project 01

---
+++ project 1.3.py
@@ -0,0 +1,11 @@
+Python 3,11,3 (Tags/v3.11.3:f3909b8, Apr  4 2023, 23:34:50) [MSC v.1934 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> age=12
+>>> print(age)
+12
+>>> weight=30
+>>> print(weight)
+30
+>>> height=230
+>>> print(height)
+230
